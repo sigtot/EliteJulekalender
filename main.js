06c9b5b9585d08126d83d8939d1d4d4e449d44b3
sigtot <sigurdtotland@gmail.com>
2016-11-28 00:06:49 +0100

    Mangler 2 bilder, ellers ferdig

--- main.js
+++ main.js
@@ -1,4 +1,36 @@
+var innerWrapper = document.getElementById("innerInnerWrapper");
+
+var d = new Date();
+//var n = d.getDate();
+n = 12;
+
 window.onload = function(){
 	// Kjører når dokumentet er lastet
-	console.log("JavaScript lastet"); // Skriv til konsollen
+	console.log("God jul, din pikk"); // Skriv til konsollen
+	for(var i = 1; i <= 24; i++){
+		var luke = document.createElement("div");
+		luke.id = "luke" + i;
+		var lukeText = document.createTextNode(i);
+		luke.appendChild(lukeText);
+		var cls = "luke";
+		if (n >= i) cls += " openable";
+		luke.className = cls;
+		luke.setAttribute("onclick","openLuke(this)")
+		innerWrapper.appendChild(luke);
+	}
+}
+
+function openLuke(self){
+	today = self.id.substr(4);
+	console.log(n);
+	console.log(today);
+	if (today <= n){
+		self.style.backgroundImage = "url(img/" + today + ".jpg)";
+		self.setAttribute("onclick","expand(this)");
+	}
+}
+
+function expand(self){
+	today = self.id.substr(4);
+	open(window.location.href + "img/" + today + ".jpg");
 }
